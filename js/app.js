f3ac4bce88a19b32277fd9d20fa6b10e4072f2bf
Olav Schuermann <developer@netzpluslogik.de>
2019-02-07 12:35:06 +0100

    Version 3 of Cat Clicker

--- js/app.js
+++ js/app.js
@@ -1,35 +1,46 @@
-let clickArea = document.getElementById('cat-image');
-let clickArea2 = document.getElementById('cat-image2');
 let counter = 0;
 
-const catNames = ['Mietze', 'Chewie', 'Schmurtel', 'Mini', 'Nera'];
-let ul = document.getElementById('catNamesList');
-let li = "";
-let theCat = "";
+const catNames = [
+    ['Mietze', 'img/cat01.jpg'], 
+    ['Chewie', 'img/cat02.jpg'], 
+    ['Schmurtel', 'img/cat03.jpg'], 
+    ['Mini', 'img/cat04.jpg'], 
+    ['Nera', 'img/cat05.jpg'], 
+];
+
+// Sort array (0 = cat name) by name
+catNames.sort(function(a, b){
+    if(a[0] < b[0]) { return -1; }
+    if(a[0] > b[0]) { return 1; }
+    return 0;
+})
+
+console.log(catNames);
 
 // Version 3 with a list of at least 5 cats, listed by name + an area to display the selected cat
+// List the cats by name in an ul-list, left side
 catNames.forEach(function(cat) {
-    console.log(cat);
+    let ul = document.getElementById('catNamesList');
+    // console.log(cat);
 
-    li = document.createElement("LI");                 // Create a <li> node
-    theCat = `${cat}`;
+    let li = document.createElement("LI");                 // Create a <li> node
+    let theCat = `${cat[0]}`;
     li.appendChild(document.createTextNode(theCat));
     ul.appendChild(li);
-})
 
-/*
-const catName1 = document.getElementById('cat1').innerHTML = `This is: ${catNames[0]}`;
-const catName2 = document.getElementById('cat2').innerHTML = `This is: ${catNames[1]}`;
+    li.addEventListener('click', function() {
+        console.log('Clicked!');
+        let clickedCatName = document.getElementById('catListName');
+        clickedCatName.innerHTML = `${cat[0]}`;
+        // console.log(clickedCat);
 
-clickArea.addEventListener('click', function() {
-    counter += 1;
-    let showClick = document.getElementById('clicks');
-    showClick.innerHTML = `Your clicks: ${counter}`;
-})
+        let clickedCatImage = document.getElementById('catListImage');
+        clickedCatImage.innerHTML = `<img src="${cat[1]}">`;
 
-clickArea2.addEventListener('click', function() {
-    counter += 1;
-    let showClick = document.getElementById('clicks');
-    showClick.innerHTML = `Your clicks: ${counter}`;
+        // Setting counter +1 for each click
+        counter += 1;
+        let showClick = document.getElementById('clicks');
+        showClick.innerHTML = `Your clicks: ${counter}`;
+
+    })
 })
-*/
