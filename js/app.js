a6649964109f19697ed10bd2cd63cd686854d87a
Olav Schuermann <olav@Olavs-MBP.fritz.box>
2019-02-07 11:50:48 +0100

    version 3 of catclicker

--- js/app.js
+++ js/app.js
@@ -2,8 +2,25 @@ let clickArea = document.getElementById('cat-image');
 let clickArea2 = document.getElementById('cat-image2');
 let counter = 0;
 
-const catNames = ['Mietze', 'Chewie'];
+const catNames = ['Mietze', 'Chewie', 'Schmurtel', 'Mini', 'Nera'];
+let ul = document.getElementById('catNamesList');
+let li = "";
+let theCat = "";
 
+// Version 3 with a list of at least 5 cats, listed by name + an area to display the selected cat
+catNames.forEach(function(cat) {
+    console.log(cat);
+
+    li = document.createElement("LI");                 // Create a <li> node
+    theCat = `${cat}`;
+    li.appendChild(document.createTextNode(theCat));
+    ul.appendChild(li);
+    //let textnode = document.createTextNode = `${cat}`;         // Create a text node
+    //node.appendChild(textnode);                              // Append the text to <li>
+    //cats.appendChild(node);
+})
+
+/*
 const catName1 = document.getElementById('cat1').innerHTML = `This is: ${catNames[0]}`;
 const catName2 = document.getElementById('cat2').innerHTML = `This is: ${catNames[1]}`;
 
@@ -18,3 +35,4 @@ clickArea2.addEventListener('click', function() {
     let showClick = document.getElementById('clicks');
     showClick.innerHTML = `Your clicks: ${counter}`;
 })
+*/
